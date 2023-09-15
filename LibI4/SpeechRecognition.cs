713eaf51c3e925000f9800e649e2819b3b0c8f5c
Alcoft <101468238+alcoftTAO@users.noreply.github.com>
2023-09-15 18:36:08 +0200

    Add files via upload

---
+++ LibI4/SpeechRecognition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TAO.I4.PythonManager;
+
+namespace TAO.I4
+{
+    public static class SpeechRecognition
+    {
+        public static string Model = "tiny";
+
+        public static string Recognize(string Language, int MaxTime = 5)
+        {
+            string data = PythonDownloader.ExecutePythonCode("whisper_recognition.py",
+                "127.0.0.1 " + PyAIResponse.ReadKeyFromFile() + " " + Language + " " + MaxTime + " " + Model
+            ).Item1;
+
+            return data;
+        }
+    }
+}
